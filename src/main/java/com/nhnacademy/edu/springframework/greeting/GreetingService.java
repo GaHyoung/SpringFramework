8a12616d8efa661b634a4e9388a39e9d992776b9
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-01-30 10:20:22 +0900

    필드 @Autowired

--- src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
+++ src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Required;
 import java.util.List;
 
 public class GreetingService {
+    //setter 메소드가 없더라도 field 에 직접 @Autowired 를 설정하여 의존성을 주입 가능.
+    @Autowired
     private Greeter greeter;
 
     public GreetingService(Greeter greeter){
@@ -21,11 +23,14 @@ public class GreetingService {
     //@Required 어노테이션은 빈의 설정(XML이나 Java Config)에서 필수적으로 프로퍼티를 설정해야 함을 나타내기 위해 사용
     // 의존성 주입이 반드시 성공해야 한다는 보장이 필요한 setter 메소드에 @Required 를 설정
     // 주로 프로퍼티에 대한 의존성을 강제할 때 사용
-    @Required
-    public void setKoreanGreeter(Greeter greeter){
-        System.out.println("setGreeter invoked");
-        this.greeter = greeter;
-    }
+//    @Required
+
+    // setter 메소드에 @Autowired 를 설정하여 의존성을 주입
+//    @Autowired
+//    public void setKoreanGreeter(Greeter greeter){
+//        System.out.println("setGreeter invoked");
+//        this.greeter = greeter;
+//    }
 
     public void greet() {
         greeter.sayHello();

a24cfa03b67c28581db9373fc01bb48312e54ad4
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-01-31 12:55:25 +0900

    Spring AOP - MessageSender 실행시간로깅 실습

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/MyStopWatch.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/MyStopWatch.java
@@ -15,9 +15,12 @@ import org.springframework.util.StopWatch;
 // execution : 메소드 실행 조인포인트와 매칭
 public class MyStopWatch {
 
+    //메서드 실행 시간을 측정할 메서드를 선택하기 위한 지점(포인트컷)을 정의
     @Pointcut("execution(public void com.nhnacademy.edu.springframework.messagesender.MessageSendService.doMessage(User, String)) && args(user, message)")
     public void messageSendService(User user, String message) {}
 
+    //어드바이스의 종류 중 하나로, 선택된 포인트컷 주변에 공통 로직을 삽입합니다.
+    //printExecutionTime 메서드는 doMessage 메서드 실행 전과 후에 특정 동작을 수행
     @Around("messageSendService(user, message)")
     public Object printExecutionTime(ProceedingJoinPoint joinPoint, User user, String message) throws Throwable {
         StopWatch stopWatch = new StopWatch();
@@ -25,8 +28,8 @@ public class MyStopWatch {
 
         try{
             //Around Advice 는 JoinPoint의 서브클래스인 ProceedingJoinPoint 를 반드시 사용
-            Object result = joinPoint.proceed();
-            return result; //MessageSender method 실행
+            Object result = joinPoint.proceed(); //타겟 메서드를 실행
+            return result;
         }catch (Throwable e){
             System.out.println(e.getMessage());
             throw e;
